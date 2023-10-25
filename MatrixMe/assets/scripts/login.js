805a6d58de055a4a182fb19ab3b4f95d106327b8
MatrixMe00 <safosah00@gmail.com>
2023-10-25 20:34:29 +0000

    Initial Commit
    Created the cart handler and a simple user login authentication

---
+++ MatrixMe/assets/scripts/login.js
@@ -0,0 +1,60 @@
+const users = [
+    {"username" : "admin", "password" : "admin"},
+    {"username" : "username", "password" : "password"}
+];
+let timer = null;
+
+$("form").submit(function(e){
+    e.preventDefault();
+    const username = $("#username").val();
+    const password = $("#password").val();
+
+    let message = ""; let type = "error";
+
+    if(username == ""){
+        message = "No username was provided";
+    }else if(password == ""){
+        message = "No password was provided";
+    }else if(!checkUsername(username)){
+        message = "Username provided is invalid";
+    }else if(!checkPassword(username, password)){
+        message = "Password is incorrect";
+    }else{
+        message = "Your login credentials are a match";
+        type = "success";
+    }
+
+    showMessage(message, type);
+})
+
+function showMessage(message, type){
+    $("#message").html(message)
+    $("#form_box").removeClass("success error no_disp").addClass(type)
+
+    clearTimeout(timer);
+
+    timer = setTimeout(function(){
+        $("#message").html("");
+        $("#form_box").removeClass(type).addClass("no_disp");
+    }, 3000)
+}
+
+function checkUsername(username) {
+    // Check if the provided username exists in the users array
+    const user = users.find(user => user.username.toLowerCase() === username.toLowerCase());
+    
+    return user ? true : false;
+}
+
+function checkPassword(username, password) {
+    // Find the user with the provided username
+    const user = users.find(user => user.username.toLowerCase() === username.toLowerCase());
+
+    if (user) {
+        // Check if the provided password matches the user's password
+        return user.password === password;
+    }
+
+    // Username not found, so password check fails
+    return false;
+}
